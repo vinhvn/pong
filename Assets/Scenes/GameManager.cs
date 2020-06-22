2305d924ace360ac11485626f92b9567b633b451
vincenthnguyen <vinhhnguyen@cmail.carleton.ca>
2020-06-21 20:53:16 -0400

    added initial assets

---
+++ Assets/Scenes/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Game objects
+    public Ball ball;
+    public Paddle paddle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(ball);
+        Instantiate(paddle);
+    }
+}
